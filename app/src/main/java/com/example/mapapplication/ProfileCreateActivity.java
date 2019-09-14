6e5832ef1a8374b15ba8a3813f81171b048b7599
Ricky <54968551+Ricky77768@users.noreply.github.com>
2019-09-13 23:28:59 -0400

    continued to implement profile (icon from taking/choosing picture)

--- app/src/main/java/com/example/mapapplication/ProfileCreateActivity.java
+++ app/src/main/java/com/example/mapapplication/ProfileCreateActivity.java
@@ -2,16 +2,23 @@ package com.example.mapapplication;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.ImageView;
+import java.io.IOException;
 
 public class ProfileCreateActivity extends AppCompatActivity {
+    final int REQUEST_IMAGE_CAPTURE = 1;
+    final int PICK_IMAGE_REQUEST = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +26,7 @@ public class ProfileCreateActivity extends AppCompatActivity {
         setContentView(R.layout.profile_create_ui);
         getSupportActionBar().hide();
 
-        // TODO: Like/Dislike - Chip/ChipGroup, Profile Picture - Select/Take a Picture
+        // TODO: Like/Dislike - Chip/ChipGroup
 
         // EventListener for Buttons
         Button profile_create_save = findViewById(R.id.profile_create_save);
@@ -36,6 +43,7 @@ public class ProfileCreateActivity extends AppCompatActivity {
             }
         });
 
+        ImageView profile_create_icon = findViewById(R.id.profile_create_icon);
         CheckBox profile_create_checkbox_time = findViewById(R.id.profile_create_checkbox_time);
         CheckBox profile_create_checkbox_budget = findViewById(R.id.profile_create_checkbox_budget);
         final EditText profile_create_time = findViewById(R.id.profile_create_time);
@@ -63,7 +71,73 @@ public class ProfileCreateActivity extends AppCompatActivity {
             }
         });
 
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        profile_create_icon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String[] options = {"Take a Picture", "Choose from Album"};
+                builder.setTitle("Upload Profile Picture");
+                builder.setItems(options, new DialogInterface.OnClickListener() {
 
+                    @Override
+                    public void onClick(DialogInterface dialog, int selected) {
+                        if (selected == 0) {
+                            takePicture();
+                        } else {
+                            choosePicture();
+                        }
+                    }
+                });
+
+                builder.show();
+            }
+        });
+
+    }
+
+    // For taking picture
+    public void takePicture() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    // For choosing picture from album
+    public void choosePicture() {
+        Intent intent = new Intent();
+
+        // Show only images, no videos or anything else
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+
+        // Always show the chooser (if there are multiple options available)
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+
+    // To handle picture selection
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+
+            ImageView profile_create_icon = findViewById(R.id.profile_create_icon);
+            profile_create_icon.setImageBitmap(imageBitmap);
+        }
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK) {
+            Uri uri = data.getData();
+
+            try {
+                // TODO: Need to compress chosen image to prevent lag
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                ImageView profile_create_icon = findViewById(R.id.profile_create_icon);
+                profile_create_icon.setImageBitmap(bitmap);
+            } catch (IOException e) { }
+
+        }
     }
 
     @Override
