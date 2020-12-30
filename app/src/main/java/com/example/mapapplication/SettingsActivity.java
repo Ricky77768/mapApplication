19576a8d806a398745bff88d42177decef57f673
Ricky77768 <rickylu77768@gmail.com>
2020-12-30 16:54:45 -0500

    Added test button to launch Google Maps externally

--- app/src/main/java/com/example/mapapplication/SettingsActivity.java
+++ app/src/main/java/com/example/mapapplication/SettingsActivity.java
@@ -2,10 +2,13 @@ package com.example.mapapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 import com.google.android.gms.maps.GoogleMap;
@@ -25,6 +28,20 @@ public class SettingsActivity extends AppCompatActivity implements AdapterView.O
         setting_map_type.setAdapter(adapter);
         setting_map_type.setOnItemSelectedListener(this);
         setting_map_type.setSelection(MapsActivity.map.getMapType() - 1);
+
+        // Test - Launch Google Maps
+        Button launch_map_test = findViewById(R.id.launch_map_test);
+        launch_map_test.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Uri gmmIntentUri = Uri.parse("https://www.google.ca/maps/dir/44.8535753,4.4061077/44.9511647,5.2237887/44.4798867,5.5204438/44.6672318,6.008371/");
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+
+                if (mapIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(mapIntent);
+                }
+            }
+        });
     }
 
     @Override // Switches map type/search radius
